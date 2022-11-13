8a9dd64629f4babe30bdc69ac448dbd85d93d11a
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-14 00:16:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
-fi
-cd /EvaMaria
+echo "Cloning main Repository"
+git clone https://github.com/maullikpatell/xomotozdi.git /xomotozdi
+cd /xomotozdi
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
