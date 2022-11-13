9ee389ff504d7ffd504bf71063e5878ffdc44915
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-14 00:33:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning main Repository"
-git clone https://github.com/maullikpatell/xomotozdi.git /EvaMaria
-cd /EvaMaria
+git clone https://github.com/maullikpatell/xomotozdi.git /xomotozdi
+cd /xomotozdi
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
