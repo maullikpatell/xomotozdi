565ddab58f895c82d53588b3c4989e653631e64c
Abir Hasan <hackitabirhasan@gmail.com>
2022-11-14 01:18:28 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 echo "Cloning main Repository"
 git clone https://github.com/maullikpatell/xomotozdi.git /xomotozdi
 cd /xomotozdi
-pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
